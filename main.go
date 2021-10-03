91103a3ac6d7ff42c578b370e6d3016c997db04d
Jonathan Acetolyne Langlois <acetolyne@programmer.net>
2021-10-03 16:50:21 -0700

    Fix folder argument

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	outputFlag := flag.String("-o", "", "Optional output file to dump results to, note output will still be shown on terminal.")
 	matchFlag := flag.String("-m", "//@todo", "The string to match to do items on, defaults to //@todo")
 
-	fmt.Println(folderFlag)
+	fmt.Println("Folder: " + *folderFlag)
 	fmt.Println("Output: " + *outputFlag)
 	fmt.Println("Match: " + *matchFlag)
 
